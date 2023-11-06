429d881faf901f241e46fe2c633ea3597d0b1267
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-11-06 01:36:39 -0300

    [Feat] - Get title and image

--- webscraping_steam_test/main.py
+++ webscraping_steam_test/main.py
@@ -1,14 +1,39 @@
 from playwright.sync_api import sync_playwright
+from selectolax.parser import HTMLParser
 
-URL_STEAM = 'https://store.steampowered.com/search/?specials'
+URL_STEAM = 'https://store.steampowered.com/specials'
 
 if __name__ == '__main__':
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
         page.goto(URL_STEAM)
 
-        page.screenshot(path='steam.png', full_page=True)
+        page.wait_for_load_state('networkidle')
+        page.wait_for_load_state('load')
+
+        #scroll to the bottom of the page
+        page.evaluate('() => window.scrollTo(0, document.body.scrollHeight)')
+        page.wait_for_load_state('domcontentloaded')
+
+        page.wait_for_selector('[class^="salepreviewwidgets_StoreSaleWidgetContainer"]')
+
+        # page.screenshot(path='steam.png', full_page=True)
+        html = page.inner_html('body')
+        tree = HTMLParser(html)
+
+        divs = tree.css('[class^="salepreviewwidgets_StoreSaleWidgetContainer"]')
+
+        for d in divs:
+            title = d.css_first('[class^="salepreviewwidgets_StoreSaleWidgetTitle"]').text()
+            thumbnail = d.css_first('img[class^="salepreviewwidgets_CapsuleImage"]').attributes['src']
+
+            print(title)
+            print(thumbnail)
+
+            attrs = {
+                'title': title,
+                'thumbnail': thumbnail
+            }
+
         browser.close()
-
-
