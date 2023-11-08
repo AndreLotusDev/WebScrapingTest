3ffbc0a8d2c91e79e84af3c7890bb7b931e5f7be
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-11-07 22:44:53 -0300

    [Feat] - Added transform for specific tags

--- webscraping_steam_test/utils/process.py
+++ webscraping_steam_test/utils/process.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from selectolax.parser import Node
 
 def get_attrs_from_node(node: Node, attr: str):
@@ -6,9 +8,24 @@ def get_attrs_from_node(node: Node, attr: str):
 
     return None
 
+def get_first_n(input_list: list, n: int):
+    if input_list:
+        value_to_return = input_list[:n]
+        return value_to_return
+
+    return None
+
+def reformat_date(date: str, old_format: str, new_format: str):
+    if date:
+        return datetime.strptime(date, old_format).strftime(new_format)
+
+    return None
+
 def format_and_transform(attrs: dict):
     transforms = {
-        "thumbnail": lambda n: get_attrs_from_node(n, "src")
+        "thumbnail": lambda n: get_attrs_from_node(n, "src"),
+        "tags": lambda input_list: get_first_n(input_list, 5),
+        "release_date": lambda date: reformat_date(date, "%b %d, %Y", "%Y-%m-%d"),
     }
 
     for k, v in transforms.items():
