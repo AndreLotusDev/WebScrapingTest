42e00bd6b95280e5b1965592ada1334fd3a6474f
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-11-21 15:11:11 -0300

    Feat: finished example with CSS

--- webscraping_scrapy/countries_gdp/countries_gdp/spiders/gdp.py
+++ webscraping_scrapy/countries_gdp/countries_gdp/spiders/gdp.py
@@ -8,5 +8,11 @@ class GdpSpider(scrapy.Spider):
 
     def parse(self, response):
         # print each row of tr table in wikipedia, using css selector function
-        for row in response.css("table.wikitable tr"):
-            print(row)
+        for country in response.css("table.wikitable.sortable tbody tr"):
+
+            yield {
+                "country_name": country.css("td:nth-child(1) a::text").get(),
+                "region": country.css("td:nth-child(2) a::text").get(),
+                "gdp": country.css("td:nth-child(3) ::text").get(),
+                "population": country.css("td:nth-child(4) ::text").get(),
+            }
