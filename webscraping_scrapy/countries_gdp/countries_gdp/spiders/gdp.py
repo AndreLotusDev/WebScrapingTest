848b252ee034af1e8fd03481654265e995deea6c
Andre Soares Gomes <andrsoares953@yahoo.com>
2023-11-21 21:03:15 -0300

    Feat: Added CSV file

--- webscraping_scrapy/countries_gdp/countries_gdp/spiders/gdp.py
+++ webscraping_scrapy/countries_gdp/countries_gdp/spiders/gdp.py
@@ -8,20 +8,11 @@ class GdpSpider(scrapy.Spider):
 
     def parse(self, response):
         # print each row of tr table in wikipedia, using css selector function
-        # for country in response.css("table.wikitable.sortable tbody tr"):
-        #
-        #     yield {
-        #         "country_name": country.css("td:nth-child(1) a::text").get(),
-        #         "region": country.css("td:nth-child(2) a::text").get(),
-        #         "gdp": country.css("td:nth-child(3) ::text").get(),
-        #         "population": country.css("td:nth-child(4) ::text").get(),
-        #     }
+        for country in response.css("table.wikitable.sortable tbody tr"):
 
-        # get using xpath
-        for country in response.xpath("//table[@class='wikitable sortable']/tbody/tr"):
             yield {
-                "country_name": country.xpath("td[1]/a/text()").get(),
-                "region": country.xpath("td[2]/a/text()").get(),
-                "gdp": country.xpath("td[3]/text()").get(),
-                "population": country.xpath("td[4]/text()").get(),
+                "country_name": country.css("td:nth-child(1) a::text").get(),
+                "region": country.css("td:nth-child(2) a::text").get(),
+                "gdp": country.css("td:nth-child(3) ::text").get(),
+                "population": country.css("td:nth-child(4) ::text").get(),
             }
