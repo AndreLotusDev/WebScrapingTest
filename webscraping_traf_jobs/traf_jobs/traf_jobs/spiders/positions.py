00cec9df6098cb3663fe214db730ca01f99879d2
Andre Soares Gomes <a.sgomes@cf-partners.com>
2023-11-27 01:37:46 -0300

    Feat: added config to playwright spider

--- webscraping_traf_jobs/traf_jobs/traf_jobs/spiders/positions.py
+++ webscraping_traf_jobs/traf_jobs/traf_jobs/spiders/positions.py
@@ -1,4 +1,7 @@
+from typing import Iterable
+
 import scrapy
+from scrapy import Request
 
 
 class PositionsSpider(scrapy.Spider):
@@ -6,10 +9,33 @@ class PositionsSpider(scrapy.Spider):
     allowed_domains = ["traf.com"]
     start_urls = ["https://trafigura.wd3.myworkdayjobs.com/TrafiguraCareerSite"]
 
-    def parse(self, response):
-        for job in response.css('section#results div[role="list"] div[role="listitem"]'):
-            yield {
-                'title': job.css('div[role="heading"]::text').get(),
-                'location': job.css('div[role="note"]::text').get(),
-                'link': job.css('a::attr(href)').get()
+    def start_requests(self):
+        yield scrapy.Request(
+            self.start_urls[0],
+            meta={
+                'playwright': True,
+                'playwright_include_page': True,
+                'playwright_page_methods': [
+                    ('wait_for_timeout', '5000'),
+                ]
             }
+        )
+
+    def parse(self, response):
+        job_results_section = response.css('section[data-automation-id="jobResults"]')
+        print(response.body)
+        print(job_results_section)
+
+        jobs_list = job_results_section.xpath('.//ul[@aria-label="Page 1 of 5"]')
+        print(jobs_list)
+
+        for job in jobs_list.xpath('./li'):
+            yield {
+                'title': job.xpath('.//a[@data-automation-id="jobTitle"]/text()').get(),
+                'location': self.extract_with_label(job, 'locations'),
+                'posted': self.extract_with_label(job, 'posted on'),
+            }
+
+            def extract_with_label(self, job, label):
+                return job.xpath(f'.//dt[normalize-space(text())="{label}"]/following-sibling::dd[1]/text()').get()
+
